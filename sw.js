750cec77f44c855cec54dd38501280b0aaddc9b0
Ezequiel Cleber Miranda <80333834+dev-zeq@users.noreply.github.com>
2026-09-26 09:39:07 -0300

    Atualiza cache dos orçamentos

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Ao publicar uma versão nova, altere somente esta constante e use o mesmo valor nos links dos HTML.
-const APP_VERSION = "20260922a";
+const APP_VERSION = "20260926a";
 const CACHE_NAME = `bezclean-painel-${APP_VERSION}`;
 const asset = path => `${path}?v=${APP_VERSION}`;
 const APP_SHELL = [
